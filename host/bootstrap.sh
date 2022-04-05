16e86baae4aefafa03ff975951ed0129957b840a
anton.bashirov <anton.sh.local@gmail.com>
2022-04-06 00:13:58 +0300

    refactoring

--- host/bootstrap.sh
+++ host/bootstrap.sh
@@ -4,7 +4,7 @@ if [[ $(id -u) -ne 0 ]] ; then
   echo "Use sudo to run this script"
 else
   if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "alpine" ]; then
-    apk add wget rsync tar aptitude iproute2 iftop git
+    apk add wget rsync tar aptitude iproute2 iftop git bsdmainutils
     wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
     tar xf nnn-musl-static-4.4.x86_64.tar.gz
     mv nnn-musl-static /usr/bin/files
