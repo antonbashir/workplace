9ec4e4e48de840a9bc5900e00233f4fad7eac9e2
anton.bashirov <anton.sh.local@gmail.com>
2022-04-05 23:43:52 +0300

    refactoring

--- common/prompt.sh
+++ common/prompt.sh
@@ -13,7 +13,7 @@ function prompt_label() {
         local host_part="$user_host_color\H"
         local user_host_part="$user_part$sign_color@$host_part"
         local directory_part="$directory_color\w"
-        local architecture_part="$architecture_color$(dpkg --print-architecture)"
+        local architecture_part="$architecture_color$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)-$(uname -m)"
         local command_part="$sign_color($(echo -e '🦊'))$white_color"
 
         PS1="$time_part $user_host_part $architecture_part $directory_part$git_color\$GIT_BRANCH\n$command_part "
@@ -31,7 +31,7 @@ function prompt_label() {
         local user_part="$user_host_color\u"
         local host_part="$user_host_color\H"
         local user_host_part="$user_part$sign_color@$host_part"
-        local architecture_part="$architecture_color$(dpkg --print-architecture)"
+        local architecture_part="$architecture_color$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)-$(uname -m)"
         local directory_part="$directory_color\w"
         local command_part="$sign_color($(echo -e '🐺'))$white_color"
 
