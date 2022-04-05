447db0ada763332f48e46f75a7ab336cce16021c
anton.bashirov <anton.sh.local@gmail.com>
2022-04-05 23:57:25 +0300

    refactoring

--- container/bootstrap.debian.sh
+++ container/bootstrap.debian.sh
@@ -2,32 +2,31 @@
 
 if [[ $(id -u) -ne 0 ]] ; then
   echo "Use sudo to run this script"
-  exit 1;
+else
+  useradd -m -s /bin/bash developer
+  passwd developer
+
+  apt update
+  apt install -y aptitude
+  aptitude install -y sudo nano htop iftop rsync tree git wget curl ssh iproute2
+  wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
+  tar xf nnn-musl-static-4.4.x86_64.tar.gz
+  mv nnn-musl-static /usr/bin/files
+  rm -rf nnn-musl-static-4.4.x86_64.tar.gz
+
+  service ssh restart
+
+  rm -rf /home/developer/.bashrc.d
+  git clone https://github.com/antonbashir/local-linux "/home/developer/.bashrc.d"
+  cp "/home/developer/.bashrc.d/container/container.sh" "/home/developer/.bashrc"
+
+  rm -rf "$HOME/.bashrc.d"
+  git clone https://github.com/antonbashir/local-linux "$HOME/.bashrc.d"
+  cp "$HOME/.bashrc.d/container/container.sh" "$HOME/.bashrc"
+
+  echo "developer ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/username
+  chown -R developer:developer /home/developer
+
+  chmod +x /root/.bashrc
+  chmod +x /home/developer/.bashrc
 fi
-
-useradd -m -s /bin/bash developer
-passwd developer
-
-apt update
-apt install -y aptitude
-aptitude install -y sudo nano htop iftop rsync tree git wget curl ssh iproute2
-wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
-tar xf nnn-musl-static-4.4.x86_64.tar.gz
-mv nnn-musl-static /usr/bin/files
-rm -rf nnn-musl-static-4.4.x86_64.tar.gz
-
-service ssh restart
-
-rm -rf /home/developer/.bashrc.d
-git clone https://github.com/antonbashir/local-linux "/home/developer/.bashrc.d"
-cp "/home/developer/.bashrc.d/container/container.sh" "/home/developer/.bashrc"
-
-rm -rf "$HOME/.bashrc.d"
-git clone https://github.com/antonbashir/local-linux "$HOME/.bashrc.d"
-cp "$HOME/.bashrc.d/container/container.sh" "$HOME/.bashrc"
-
-echo "developer ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/username
-chown -R developer:developer /home/developer
-
-chmod +x /root/.bashrc
-chmod +x /home/developer/.bashrc
--- host/bootstrap.sh
+++ host/bootstrap.sh
@@ -2,25 +2,24 @@
 
 if [[ $(id -u) -ne 0 ]] ; then
   echo "Use sudo to run this script"
-  exit 1;
-fi
+else
+  if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "alpine" ]; then
+    apk add wget rsync tar aptitude iproute2 iftop git
+    wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
+    tar xf nnn-musl-static-4.4.x86_64.tar.gz
+    mv nnn-musl-static /usr/bin/files
+    rm -rf nnn-musl-static-4.4.x86_64.tar.gz
+  fi
 
-if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "alpine" ]; then
-  apk add wget rsync tar aptitude iproute2 iftop git
-  wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
-  tar xf nnn-musl-static-4.4.x86_64.tar.gz
-  mv nnn-musl-static /usr/bin/files
-  rm -rf nnn-musl-static-4.4.x86_64.tar.gz
-fi
+  if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "debian" ]; then
+    apt install -y git wget rsync tar aptitude iproute2 iftop
+    wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
+    tar xf nnn-musl-static-4.4.x86_64.tar.gz
+    mv nnn-musl-static /usr/bin/files
+    rm -rf nnn-musl-static-4.4.x86_64.tar.gz
+  fi
 
-if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "debian" ]; then
-  apt install -y git wget rsync tar aptitude iproute2 iftop
-  wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
-  tar xf nnn-musl-static-4.4.x86_64.tar.gz
-  mv nnn-musl-static /usr/bin/files
-  rm -rf nnn-musl-static-4.4.x86_64.tar.gz
+  rm -rf "$HOME/.bashrc.d"
+  git clone https://github.com/antonbashir/local-linux "$HOME/.bashrc.d"
+  cp "$HOME/.bashrc.d/host/host.sh" "$HOME/.bashrc"
 fi
-
-rm -rf "$HOME/.bashrc.d"
-git clone https://github.com/antonbashir/local-linux "$HOME/.bashrc.d"
-cp "$HOME/.bashrc.d/host/host.sh" "$HOME/.bashrc"
