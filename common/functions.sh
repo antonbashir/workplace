68efc458263da3036e0fa4d5337066e532b308b2
anton.bashirov <anton.sh.local@gmail.com>
2022-04-05 23:34:45 +0300

    refactoring

--- common/functions.sh
+++ common/functions.sh
@@ -19,19 +19,3 @@ toFile() {
 toDir() {
         sudo chmod 755 $1
 }
-
-
-prepare() {
-    if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "alpine" ]; then
-      alias package='sudo apk add wget rsync tar aptitude iproute2 iftop'
-    fi
-
-    if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "debian" ]; then
-      sudo apt install -y wget rsync tar aptitude iproute2 iftop
-    fi
-
-    sudo wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
-    sudo tar xf nnn-musl-static-4.4.x86_64.tar.gz
-    sudo mv nnn-musl-static /usr/bin/files
-    sudo rm -rf nnn-musl-static-4.4.x86_64.tar.gz
-}
--- host/bootstrap.sh
+++ host/bootstrap.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "alpine" ]; then
-  sudo apk add git
+  sudo apk add wget rsync tar aptitude iproute2 iftop git
+  sudo wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
+  sudo tar xf nnn-musl-static-4.4.x86_64.tar.gz
+  sudo mv nnn-musl-static /usr/bin/files
+  sudo rm -rf nnn-musl-static-4.4.x86_64.tar.gz
 fi
 
 if [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) == "debian" ]; then
-  sudo apt install git
+  sudo apt install -y git wget rsync tar aptitude iproute2 iftop
+  sudo wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
+  sudo tar xf nnn-musl-static-4.4.x86_64.tar.gz
+  sudo mv nnn-musl-static /usr/bin/files
+  sudo rm -rf nnn-musl-static-4.4.x86_64.tar.gz
 fi
 
 git clone https://github.com/antonbashir/local-linux "$HOME/.bashrc.d"
