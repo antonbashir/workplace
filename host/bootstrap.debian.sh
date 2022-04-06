8bbcd03ea7e376637ac10d4e723754306a2e11c3
anton.bashirov <anton.sh.local@gmail.com>
2022-04-06 14:20:32 +0300

    refactoring

--- host/bootstrap.debian.sh
+++ host/bootstrap.debian.sh
@@ -22,6 +22,11 @@ sudo aptitude install -y \
       gpg \
       pass
 
+sudo wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart.gpg
+sudo echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
+sudo aptitude update
+sudo aptitude -y install dart
+
 sudo wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-musl-static-4.4.x86_64.tar.gz
 sudo tar xf nnn-musl-static-4.4.x86_64.tar.gz
 sudo mv nnn-musl-static /usr/bin/files
@@ -34,3 +39,4 @@ rm -rf "$HOME/.profile.d"
 git clone https://github.com/antonbashir/local-linux "$HOME/.profile.d"
 cp "$HOME/.profile.d/host/host.sh" "$HOME/.profile"
 
+echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> "$HOME/.profile"
