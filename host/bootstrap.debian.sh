49ff5bd06ba851c9d1c4c965c02b985854f26378
Anton Bashirov <anton.sh.local@gmail.com>
2022-04-09 01:39:23 +0300

    Update bootstrap.debian.sh

--- host/bootstrap.debian.sh
+++ host/bootstrap.debian.sh
@@ -28,7 +28,10 @@ sudo aptitude install -y \
       pass \
       sshfs \
       apt-transport-https \
-      asciinema
+      asciinema \
+      lxc \
+      lcxfs \
+      lxc-templates
 
 sudo wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart.gpg
 sudo echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
