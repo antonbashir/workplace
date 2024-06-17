b3aa8a59ebcb39a08bda235e710ad79f015dde1d
Anton Bashirov <anton.sh.local@gmail.com>
2024-06-17 21:41:06 +0400

    Update bootstrap.debian.sh

--- host/bootstrap.debian.sh
+++ host/bootstrap.debian.sh
@@ -41,6 +41,13 @@ rm -rf "$HOME/.profile.old"
 cp "$HOME/.profile" "$HOME/.profile.old"
 cp "$HOME/.profile.d/host/host.sh" "$HOME/.profile"
 
+
+if [[ $(grep microsoft /proc/version) ]]; then
+  sudo mkdir -p /sys/fs/cgroup/systemd && sudo mount -t cgroup cgroup -o none,name=systemd /sys/fs/cgroup/systemd
+  sudo mkdir -p /sys/fs/cgroup/systemd && sudo mount -t cgroup cgroup -o none,name=systemd /sys/fs/cgroup/systemd
+  sudo bash -c "echo 'cgroup2 /sys/fs/cgroup cgroup2 rw,nosuid,nodev,noexec,relatime,nsdelegate 0 0' >> /etc/fstab"
+fi
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
