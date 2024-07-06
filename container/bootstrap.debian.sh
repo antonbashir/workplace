c3fd217d18fedbb32fcc7daf5ff4bcb70b73a2a9
anton.bashirov <anton.sh.local@gmail.com>
2024-07-06 16:22:15 +0400

    fixes

--- container/bootstrap.debian.sh
+++ container/bootstrap.debian.sh
@@ -36,7 +36,7 @@ else
   
   wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg  --dearmor -o /usr/share/keyrings/dart.gpg
   echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-  sudo apt update && aptitude install dart
+  sudo apt update && sudo aptitude install dart
 
   go_archive=$(curl https://go.dev/VERSION?m=text | head -1).linux-amd64.tar.gz
   wget "https://dl.google.com/go/$go_archive"
